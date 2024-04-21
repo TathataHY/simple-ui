c3f768d9c12e2baf7c2e4684a2a861781226e462
Tathata <andre.yovera1@gmail.com>
2024-04-21 14:47:09 -0400

    Add Box and Stack components to Layout

--- src/components/Input/index.stories.ts
+++ src/components/Input/index.stories.ts
@@ -1,5 +1,5 @@
+import { Input } from "@/components";
 import type { Meta, StoryObj } from "@storybook/react";
-import { Input } from ".";
 
 const meta: Meta<typeof Input> = {
   title: "Components/Input",
---
+++ src/components/Layout/Stack/index.stories.tsx
@@ -0,0 +1,24 @@
+import { Box, Stack } from "@/components";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Stack> = {
+  title: "Layout/Stack",
+  component: Stack,
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <Stack className="gap-4 p-4 bg-gray-300" {...args}>
+      <Box className="w-[100px] h-[100px] bg-blue-500" />
+      <Box className="w-[100px] h-[100px] bg-red-500" />
+      <Box className="w-[100px] h-[100px] bg-green-500" />
+    </Stack>
+  ),
+};
